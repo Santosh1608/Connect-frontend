50c2549632a985e8ca0399545012c373a66a958c
Santhosh <nannisantosh@gmail.com>
2021-05-11 18:11:06 +0530

    Compressed original files to upload

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,16 +2,26 @@ import React, { Component } from "react";
 import axios from "axios";
 import Loading from "../Loading/Loading";
 import classes from "./Post.module.css";
-
+import Compressor from "compressorjs";
 class Post extends Component {
   state = {
     title: "",
     photo: null,
     loading: false,
+    compressedPhoto: null,
   };
   onChange = (e) => {
     if (e.target.name == "photo") {
       console.log(e.target.name);
+      const image = e.target.files[0];
+      new Compressor(image, {
+        quality: 0.5, // 0.6 can also be used, but its not recommended to go below.
+        success: (compressedResult) => {
+          // compressedResult has the compressed file.
+          // Use the compressed file to upload the images to your server.
+          this.setState({ compressedPhoto: compressedResult });
+        },
+      });
       this.setState({
         [e.target.name]: e.target.files[0],
       });
@@ -26,7 +36,7 @@ class Post extends Component {
       try {
         console.log("Sending");
         const formData = new FormData();
-        formData.append("photo", this.state.photo);
+        formData.append("photo", this.state.compressedPhoto);
         formData.append("title", this.state.title);
         this.setState({ loading: true });
         await axios({
