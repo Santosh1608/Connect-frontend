b92953f9901cdffdf438a7eb408975ee2bff7f6a
Santhosh <nannisantosh@gmail.com>
2021-05-12 11:17:10 +0530

    add UserProfile styles

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -6,11 +6,51 @@ import * as authActions from "../../actions/auth";
 class UserProfile extends Component {
   render() {
     return (
-      <div className={classes.UserProfile}>
-        <img src={this.props.user.avatar.url} />
-        <Link onClick={this.props.logout} to="#">
-          logout
-        </Link>
+      <div className={classes.User}>
+        <div className={classes.UserProfile}>
+          <div className={classes.Left}>
+            <img src={this.props.user.avatar.url} />
+          </div>
+          <div className={classes.Right}>
+            <h1>{this.props.user.name}</h1>
+            <div className={classes.Icons}>
+              <div>
+                <i class="fas fa-edit"></i>
+                <span className={classes.Edit}>Edit</span>
+              </div>
+              <div>
+                <i class="far fa-trash-alt"></i>
+                <span className={classes.Delete}>Delete Account</span>
+              </div>
+              <div>
+                <i onClick={this.props.logout} class="fas fa-sign-out-alt"></i>
+                <span className={classes.Logout}>Logout</span>
+              </div>
+            </div>
+            <div className={classes.UserData}>
+              <p>
+                <span>10</span>Posts
+              </p>
+              <p>
+                <span>100</span>Following
+              </p>
+              <p>
+                <span>50</span>Followers
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className={classes.UserDataMobile}>
+          <p>
+            <span>10</span>Posts
+          </p>
+          <p>
+            <span>100</span>Following
+          </p>
+          <p>
+            <span>50</span>Followers
+          </p>
+        </div>
       </div>
     );
   }
